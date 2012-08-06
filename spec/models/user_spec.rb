73e0b5fa8a7d958cb4924ad2a7a67f4555e09c8b
Annerose <anb128@pitt.edu>
2012-08-06 17:19:31 -0400

    Finish updating users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,22 +2,32 @@ require 'spec_helper'
 
 describe User do
   
-  before do
-  	@user = User.new(:name => "Example User", :email => "user@example.com",
-  					         :password => "foobar", :password_confirmation => "foobar")
-  end
+	before do
+		@user = User.new(:name => "Example User", :email => "user@example.com",
+						         :password => "foobar", :password_confirmation => "foobar")
+	end
 
-  subject { @user }
+	subject { @user }
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:remember_token) }
-  it { should respond_to(:authenticate) }
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
+	it { should respond_to(:admin) }
+	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
+	it {should_not be_admin }
+
+	describe "accessible attributes" do
+		it "should not allow access to admin" do
+			expect do
+				User.new(:admin => "1")
+			end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+		end
+	end
 
 	describe "when name is not present" do
 		before { @user.name = " " }
@@ -60,7 +70,6 @@ describe User do
 			user_with_same_email.email = @user.email.upcase
 			user_with_same_email.save
 		end
-
 		it { should_not be_valid }
 	end	
 
